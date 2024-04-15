2760903310aaa2c22adc1c01a0afd1e368c48243
Ghazi Nakkash <ghazi.nakkash@student.htw-Berlin.de>
2024-04-15 11:19:52 +0200

    Service Interface

--- src/main/java/de/htwberlin/maven_demo/Service.java
+++ src/main/java/de/htwberlin/maven_demo/Service.java
@@ -1,6 +1,20 @@
 package de.htwberlin.maven_demo;
 
+import javax.smartcardio.Card;
+import java.awt.*;
+
 public interface Service {
+        void initializeGame(int numberOfPlayers);
+        void startTurn(int player);
+        void playCard(Card card);
+        void drawCard(int player);
+        void nextPlayer();
+        boolean checkWinCondition(int player);
+        void shuffleDiscardPileIntoDrawPile();
+        void applySpecialCardEffects(Card card);
+        List<Card> checkPlayableCards(int player);
+        boolean checkValidCard(Card card, Card topCard);
+        void updateGameState();
+        void endGame();
+    }
 
-
-}
