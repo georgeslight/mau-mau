4e5dc04153b27202606a2808647ccc9d97283d70
George Slight <georgeslight@gmail.com>
2024-05-06 11:59:21 +0200

    small fix

--- RulesManagement/src/main/java/de/htwberlin/service/RuleService.java
+++ RulesManagement/src/main/java/de/htwberlin/service/RuleService.java
@@ -1,44 +1,37 @@
 package de.htwberlin.service;
 
 import de.htwberlin.model.Card;
-import de.htwberlin.model.Player;
 
 public class RuleService implements RuleEngineInterface {
 
     @Override
     public void applySpecialCardEffects(Card card) {
-        // To-Do
+//        todo
     }
 
     @Override
     public boolean checkValidCard(Card card, Card topCard) {
-        // To-Do
-        return false;
-    }
-
-    @Override
-    public boolean checkWinner(Player player) {
-        // To-Do
+//        todo
         return false;
     }
 
     @Override
     public void playJack() {
-        // To-Do
+//        todo
     }
 
     @Override
     public void play7() {
-        // To-Do
+//        todo
     }
 
     @Override
     public void play8() {
-        // To-Do
+//        todo
     }
 
     @Override
     public void playAce() {
-        // To-Do
+//        todo
     }
 }
