3b6f40bc6bd732a37a0e4ac9bb9c064a561bbd6a
George Slight <georgeslight@gmail.com>
2024-05-17 22:00:26 +0200

    Deck constructior changed

--- CardsManagement/src/main/java/de/htwberlin/model/Deck.java
+++ CardsManagement/src/main/java/de/htwberlin/model/Deck.java
@@ -1,22 +1,13 @@
 package de.htwberlin.model;
 
-import de.htwberlin.enums.Rank;
-import de.htwberlin.enums.Suit;
-
-import java.util.Collections;
 import java.util.Stack;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Deck {
 
     private final Stack<Card> cards;
 
-    public Deck() {
-        this.cards = Stream.of(Suit.values())
-                .flatMap(suit -> Stream.of(Rank.values())
-                        .map(rank -> new Card(suit, rank)))
-                .collect(Collectors.toCollection(Stack::new));
+    public Deck(Stack<Card> cards) {
+        this.cards = cards;
     }
 
     public Stack<Card> getCards() {
