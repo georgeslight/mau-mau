00f4caa153ced077c49c3b2225e8ab9ebf23f597
George Slight <georgeslight@gmail.com>
2024-06-06 12:01:45 +0200

    fixed GameServiceTest.setUp and GameServiceTest.testInitGame

--- GameEngine/src/test/java/de/htwberlin/service/GameServiceTest.java
+++ GameEngine/src/test/java/de/htwberlin/service/GameServiceTest.java
@@ -2,9 +2,9 @@ package de.htwberlin.service;
 
 import de.htwberlin.api.model.GameState;
 import de.htwberlin.api.model.Player;
-import de.htwberlin.api.service.GameManagerInterface;
 import de.htwberlin.impl.service.CardService;
 import de.htwberlin.impl.service.GameService;
+import de.htwberlin.impl.service.PlayerService;
 import de.htwberlin.impl.service.RuleService;
 import de.htwberlin.api.model.*;
 import de.htwberlin.api.enums.*;
@@ -12,65 +12,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class GameServiceTest {
-    private GameManagerInterface gameService;
-    private GameState gameState;
-    private Player player1;
-    private Player player2;
-    private Player player3;
-    private Player player4;
-    private RuleService ruleService;
+
+    private GameService gameService;
+    private PlayerService playerService;
     private CardService cardService;
+    private RuleService ruleService;
 
     @BeforeEach
     void setUp() {
-        cardService = new CardService();
-        gameService = new GameService();
-        ruleService = mock(RuleService.class);
-        gameState = new GameState();
-
-        player1 = new Player("Player 1", List.of(
-                new Card(Suit.HEARTS, Rank.SEVEN),
-                new Card(Suit.CLUBS, Rank.EIGHT),
-                new Card(Suit.SPADES, Rank.NINE),
-                new Card(Suit.DIAMONDS, Rank.TEN),
-                new Card(Suit.CLUBS, Rank.JACK)
-        ));
-
-        player2 = new Player("Player 2", List.of(
-                new Card(Suit.SPADES, Rank.QUEEN),
-                new Card(Suit.DIAMONDS, Rank.KING),
-                new Card(Suit.HEARTS, Rank.ACE),
-                new Card(Suit.CLUBS, Rank.SEVEN),
-                new Card(Suit.SPADES, Rank.EIGHT)
-        ));
-
-        player3 = new Player("Player 3", List.of(
-                new Card(Suit.HEARTS, Rank.NINE),
-                new Card(Suit.CLUBS, Rank.TEN),
-                new Card(Suit.SPADES, Rank.JACK),
-                new Card(Suit.DIAMONDS, Rank.QUEEN),
-                new Card(Suit.HEARTS, Rank.KING)
-        ));
-
-        player4 = new Player("Player 4", List.of(
-                new Card(Suit.DIAMONDS, Rank.ACE),
-                new Card(Suit.SPADES, Rank.SEVEN),
-                new Card(Suit.CLUBS, Rank.NINE),
-                new Card(Suit.HEARTS, Rank.TEN),
-                new Card(Suit.DIAMONDS, Rank.JACK)
-        ));
-
-
-        gameState.setPlayers(Arrays.asList(player1, player2, player3, player4));
-        gameState.setCurrentPlayerIndex(0);
-        gameState.setDeck(cardService.createDeck());
-        gameState.setDiscardPile(new Stack<>());
+        this.playerService = mock(PlayerService.class);
+        this.cardService = mock(CardService.class);
+        this.ruleService = mock(RuleService.class);
+        this.gameService = new GameService(playerService, cardService, ruleService);
     }
 
     /**
@@ -78,13 +40,34 @@ class GameServiceTest {
      */
     @Test
     void testInitGame() {
+        // Create a valid deck of cards
+        Stack<Card> deck = Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Rank.values())
+                        .map(rank -> new Card(suit, rank)))
+                .collect(Collectors.toCollection(Stack::new));
+
+        // Define the behavior of the cardService mocks
+        when(cardService.createDeck()).thenReturn(deck);
+        when(cardService.shuffle(any())).thenAnswer(invocation -> {
+            Stack<Card> originalDeck = invocation.getArgument(0);
+            Collections.shuffle(originalDeck);
+            return originalDeck;
+        });
+
+        // Define the behavior of the playerService mock to return valid players
+        when(playerService.createPlayer(anyString(), anyList())).thenAnswer(invocation -> {
+            String name = invocation.getArgument(0);
+            List<Card> hand = invocation.getArgument(1);
+            return new Player(name, hand);
+        });
+
         int playersCount = 4;
-        GameState gamestate = gameService.initializeGame(playersCount);
+        GameState gameState = gameService.initializeGame(playersCount);
         gameState.getPlayers().forEach(player -> assertEquals(5, player.getHand().size())); // checks that every player has 5 cards on hand
-        assertEquals(32 - (playersCount * 5) - 1, gamestate.getDeck().getCards().size()); // checks deck has 32 cards - 5 cards for every player - initial discardPile card
-        assertEquals(playersCount, gamestate.getPlayers().size());
-        assertNotEquals(gameState.getCurrentPlayerIndex(), gamestate.getNextPlayerIndex());
-        assertEquals(0, gamestate.getCurrentPlayerIndex());
+        assertEquals(32 - (playersCount * 5) - 1, gameState.getDeck().size()); // checks deck has 32 cards - 5 cards for every player - initial discardPile card
+        assertEquals(playersCount, gameState.getPlayers().size());
+        assertNotEquals(gameState.getCurrentPlayerIndex(), gameState.getNextPlayerIndex());
+        assertEquals(0, gameState.getCurrentPlayerIndex());
     }
 
     /**
