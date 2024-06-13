e0155e49ea1309269d25a56c3454dd1694002e52
George Slight <georgeslight@gmail.com>
2024-06-13 16:14:52 +0200

    deleted unnecessary App classes

--- GameEngine/src/main/java/de/htwberlin/App.java
+++ GameEngine/src/main/java/de/htwberlin/App.java
@@ -1,13 +1,13 @@
-package de.htwberlin;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+//package de.htwberlin;
+//
+///**
+// * Hello world!
+// *
+// */
+//public class App
+//{
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+//}
