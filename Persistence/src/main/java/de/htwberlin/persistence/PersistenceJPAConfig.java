8ff04ceae0a6c410ccc72bace1261a126184f2b9
George Slight <georgeslight@gmail.com>
2024-07-09 23:18:20 +0200

    persistence module

--- Persistence/src/main/java/de/htwberlin/persistence/PersistenceJPAConfig.java
+++ Persistence/src/main/java/de/htwberlin/persistence/PersistenceJPAConfig.java
@@ -1,4 +1,4 @@
-package de.htwberlin.configuration;
+package de.htwberlin.persistence;
 
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
