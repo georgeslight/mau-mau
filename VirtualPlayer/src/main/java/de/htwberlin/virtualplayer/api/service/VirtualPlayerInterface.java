73473156fb8f0990c898cbe26ee927da2f1630d4
Ghazi Nakkash <ghazi.nakkash@student.htw-Berlin.de>
2024-07-27 15:31:50 +0200

    virtinterface methodes

--- VirtualPlayer/src/main/java/de/htwberlin/virtualplayer/api/service/VirtualPlayerInterface.java
+++ VirtualPlayer/src/main/java/de/htwberlin/virtualplayer/api/service/VirtualPlayerInterface.java
@@ -2,11 +2,41 @@ package de.htwberlin.virtualplayer.api.service;
 
 import de.htwberlin.cardsmanagement.api.model.Card;
 import de.htwberlin.playermanagement.api.model.Player;
+import de.htwberlin.rulesmanagement.impl.RuleService;
 
 import java.util.List;
 
 public interface VirtualPlayerInterface {
 
+
     void makeMove();
+
+    /**
+     * choose a card to play
+     * @param player
+     * @param topCard
+     * @param ruleService
+     * @return the card to play
+     */
+    Card decideCardToPlay(Player player, Card topCard, RuleService ruleService);
+
+    /**
+     * decide a suit to play
+     * @param player
+     * @param ruleService
+     */
+    void decideSuit(Player player, RuleService ruleService);
+
+    /**
+     * decide weither to draw a card from the deck or not
+     * @param player
+     */
+    void drawCard(Player player);
+
+    /**
+     * decide weither to say "Mau" or not
+     * @param player
+     */
+    void sayMau(Player player);
 
 }
--- VirtualPlayer/src/main/java/de/htwberlin/virtualplayer/impl/VirtualPlayerService.java
+++ VirtualPlayer/src/main/java/de/htwberlin/virtualplayer/impl/VirtualPlayerService.java
@@ -3,12 +3,14 @@ package de.htwberlin.virtualplayer.impl;
 import de.htwberlin.cardsmanagement.api.model.Card;
 import de.htwberlin.cardsmanagement.impl.CardComparator;
 import de.htwberlin.playermanagement.api.model.Player;
+import de.htwberlin.rulesmanagement.impl.RuleService;
 import de.htwberlin.virtualplayer.api.service.VirtualPlayerInterface;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class VirtualPlayerService implements VirtualPlayerInterface {
@@ -18,7 +20,28 @@ public class VirtualPlayerService implements VirtualPlayerInterface {
 
     @Override
     public void makeMove() {
+
         LOGGER.info("Virtual player makes a move");
+    }
+
+    @Override
+    public Card decideCardToPlay(Player player, Card topCard, RuleService ruleService) {
+        return null;
+    }
+
+    @Override
+    public void decideSuit(Player player, RuleService ruleService) {
+
+    }
+
+    @Override
+    public void drawCard(Player player) {
+
+    }
+
+    @Override
+    public void sayMau(Player player) {
+
     }
 }
 
